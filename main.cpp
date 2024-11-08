117ad654cfe80ad519f7904b6c6ad1e2b0ea50c4
Al-aziz19 <st135793@student.spbu.ru>
2024-11-08 04:35:19 +0300

    new

--- main.cpp
+++ main.cpp
@@ -106,3 +106,86 @@ void BMP::Save(const std::string &filename) {
     
     file.close();
 }
+
+void BMP::Rotate90() {
+    std::vector<std::vector<Pixel>> rotatedData(infoHeader.biWidth, std::vector<Pixel>(infoHeader.biHeight));
+
+    for (int i = 0; i < infoHeader.biHeight; ++i) {
+        for (int j = 0; j < infoHeader.biWidth; ++j) {
+            rotatedData[j][infoHeader.biHeight - i - 1] = data[i][j];
+        }
+    }
+
+   data = rotatedData;
+
+   std::swap(infoHeader.biWidth, infoHeader.biHeight);
+}
+
+void BMP::RotateCounter90() {
+   std::vector<std::vector<Pixel>> rotatedData(infoHeader.biWidth, std::vector<Pixel>(infoHeader.biHeight));
+
+   for (int i = 0; i < infoHeader.biHeight; ++i) {
+       for (int j = 0; j < infoHeader.biWidth; ++j) {
+           rotatedData[infoHeader.biWidth - j - 1][i] = data[i][j];
+       }
+   }
+
+   data = rotatedData;
+
+   std::swap(infoHeader.biWidth, infoHeader.biHeight);
+}
+
+void BMP::GaussianFilter() {
+   float kernel[3][3] = {
+       {1 / 16.0f, 2 / 16.0f, 1 / 16.0f},
+       {2 / 16.0f, 4 / 16.0f, 2 / 16.0f},
+       {1 / 16.0f, 2 / 16.0f, 1 / 16.0f}
+   };
+
+   std::vector<std::vector<Pixel>> tempData(infoHeader.biHeight, std::vector<Pixel>(infoHeader.biWidth));
+
+   for (int y = 0; y < infoHeader.biHeight; ++y) {
+       for (int x = 0; x < infoHeader.biWidth; ++x) {
+           float sumRed = 0, sumGreen = 0, sumBlue = 0;
+
+           for (int ky = -1; ky <= 1; ++ky) {
+               for (int kx = -1; kx <= 1; ++kx) {
+                   int nx = x + kx;
+                   int ny = y + ky;
+
+                   nx = std::max(0, std::min(nx, infoHeader.biWidth - 1));
+                   ny = std::max(0, std::min(ny, infoHeader.biHeight - 1));
+
+                   sumRed   += data[ny][nx].rgbtRed * kernel[ky + 1][kx + 1];
+                   sumGreen += data[ny][nx].rgbtGreen * kernel[ky + 1][kx + 1];
+                   sumBlue  += data[ny][nx].rgbtBlue * kernel[ky + 1][kx + 1];
+               }
+           }
+           tempData[y][x].rgbtRed   = static_cast<uint8_t>(sumRed < 0 ? 0 : (sumRed > 255 ? 255 : sumRed));
+           tempData[y][x].rgbtGreen = static_cast<uint8_t>(sumGreen < 0 ? 0 : (sumGreen > 255 ? 255 : sumGreen));
+           tempData[y][x].rgbtBlue  = static_cast<uint8_t>(sumBlue < 0 ? 0 : (sumBlue > 255 ? 255 : sumBlue));
+       }
+   }
+
+   data = tempData;
+}
+
+int main() {
+   try {
+       BMP image("yard.bmp");
+       BMP imageCounterClockwise("yard.bmp");
+
+       image.Rotate90();
+       image.Save("output_clockwise.bmp");
+
+       imageCounterClockwise.RotateCounter90();
+       imageCounterClockwise.Save("output_counterclockwise.bmp");
+
+       image.GaussianFilter();
+       image.Save("output_gaussian.bmp");
+
+   } catch (const std::exception &e) {
+       std::cerr << "Error: " << e.what() << std::endl;
+   }
+   return 0;
+}
